c902d5996915e715970654dab937bdd69142b117
kimjuyoung <rlawn55@gmail.com>
2017-05-18 19:07:48 +0900

    사진 제대로 제 구역가고 오른쪽 위에서부터 2,3,4 디비에 저장

--- BillyU/src/main/java/regist/registDAO.java
+++ BillyU/src/main/java/regist/registDAO.java
@@ -151,9 +151,9 @@ public class registDAO {
 			PreparedStatement pstmt = conn.prepareStatement(query);
 			pstmt.setInt(1, pronum);
 			pstmt.setString(2, "resources/img/" + dto.getImg());
-			pstmt.setString(3, "resources/img/" + dto.getImg2());
+			pstmt.setString(3, "resources/img/" + dto.getImg4());
 			pstmt.setString(4, "resources/img/" + dto.getImg3());
-			pstmt.setString(5, "resources/img/" + dto.getImg4());
+			pstmt.setString(5, "resources/img/" + dto.getImg2());
 
 			pstmt.executeUpdate();
 			check = true;
