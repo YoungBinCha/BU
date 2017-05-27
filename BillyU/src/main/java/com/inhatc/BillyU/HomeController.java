625bb05dbdf99c69f5eba7788887d954746cf41c
kimjuyoung <rlawn55@gmail.com>
2017-05-27 13:17:21 +0900

    이름 다 바꿈

--- BillyU/src/main/java/com/inhatc/BillyU/HomeController.java
+++ BillyU/src/main/java/com/inhatc/BillyU/HomeController.java
@@ -56,7 +56,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTime", formattedDate ); 
 		
 		return "home";
 	}
